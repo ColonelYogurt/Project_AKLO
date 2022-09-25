3d631bab90826778a2e5e509ff02fe6da306abcc
Eric Zachariah <eric.zachri@gmail.com>
2022-09-25 18:32:35 -0400

    Add Row updates

--- addRow.py
+++ addRow.py
@@ -1,1 +1,10 @@
-from csv import writer
+#import pandas as pd
+#import random as rand
+
+#csv_input = pd.read_csv(column)
+
+#csv_input[new_column] = csv_input[rand.randint(0,1)]
+
+#csv_input[new_column_2] = csv_input[rand.randint(0,1)]
+
+#csv_input.to_csv(output_csv_file, index=False)
