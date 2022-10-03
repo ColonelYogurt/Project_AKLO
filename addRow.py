e88e9f2821f77ea6d5887ee72a287291ccab0475
Eric Zachariah <eric.zachri@gmail.com>
2022-10-03 03:08:48 -0400

    Working code

--- addRow.py
+++ addRow.py
@@ -1,10 +1,13 @@
-#import pandas as pd
-#import random as rand
+import pandas as pd
+import random as rand
 
-#csv_input = pd.read_csv(column)
+def random():
+    return rand.int(0,1)
 
-#csv_input[new_column] = csv_input[rand.randint(0,1)]
+csv_input = pd.read_csv(column)
 
-#csv_input[new_column_2] = csv_input[rand.randint(0,1)]
+csv_input['new_column'] = csv_input[random()]
 
-#csv_input.to_csv(output_csv_file, index=False)
+csv_input['new_column_2'] = csv_input[random()]
+
+csv_input.to_csv(output_csv_file, index=False)
