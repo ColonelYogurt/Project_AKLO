3133de19db569405e0c0e0427f72e6eb22ef69bf
Eric Zachariah <eric.zachri@gmail.com>
2022-10-09 19:56:36 -0400

    Mqtt skeleton

---
+++ mqttTest.py
@@ -0,0 +1,17 @@
+import paho.mqtt.client as mqtt #import the client1
+
+def on_message(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
+
+
+broker_address="192.168.1.184" 
+
+client = mqtt.Client("P1") #create new instance
+
+client.on_message=on_message
+
+client.connect(broker_address) #connect to broker
+client.publish("house/main-light","OFF")#publish
