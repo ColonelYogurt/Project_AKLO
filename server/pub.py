3f17a3767593991b451da259be5736018aa383b5
rustygate229 <karnati.10@osu.edu>
2022-10-31 22:14:24 -0400

    Send and recieve csv data

--- server/pub.py
+++ server/pub.py
@@ -1,21 +1,26 @@
 import paho.mqtt.client as mqtt #import the client1
 import time
+from csv_mqtt.csv_mqtt import CsvMqtt
 
+# constants
+csv_path = "../Assets/Test.csv" 
 mqtt_broker = 'mqtt.eclipseprojects.io'
-topic = "csv/test"
+topic = "csv/test" #topic that msg is published to
 
+#connects a client
+#client can publish csv files as json
+#returns client
 def client_connect():
-    client = mqtt.Client("Client_1")
-    client.connect(mqtt_broker)
-
+    client = CsvMqtt(mqtt_broker)
     return client
 
-def publish(client):
+#publishes csv data row by row to topic
+#every 5 seconds
+def publish_msg(client):
     while True:
-        client.publish(topic, "just a test")
-        print(f"Published to topic: {topic}")
-        time.sleep(1)
+        client.publish_csv_data(csv_path, topic)
+        time.sleep(5)
 
 if __name__ == "__main__":
     client = client_connect()
-    publish(client)
+    publish_msg(client)
--- server/sub.py
+++ server/sub.py
@@ -1,29 +1,38 @@
-import time
+import re
 import paho.mqtt.client as mqtt
+import json
 
+#constants
 mqtt_broker = 'mqtt.eclipseprojects.io'
 topic = "csv/test"
 
-def on_message(client, userdata, message):
-    print("received message: " ,str(message.payload.decode("utf-8")))
-
-def client_connect():
-    client = mqtt.Client("Client_2")
-    client.connect(mqtt_broker)
-
+#connects a client as a Client object
+#returns that client
+def connect_mqtt():
+    client = mqtt.Client()
+    client.connect(mqtt_broker, 1883, 60)
     return client
 
-def subscribe(client):
-
-    client.loop_start()
-
+#function that runs when client connects to broker
+def on_connect(client, userdata, flags, rc):
+    print("Successfully connected with result code "+str(rc))
     client.subscribe(topic)
-    client.on_message=on_message
 
-    time.sleep(5)
+#function that runs when message is received
+#outputs sensor and transducer data
+def on_message(client, userdata, msg):
 
-    client.loop_stop()
+    #receives payload
+    res = msg.payload
+    final_res = json.loads(res.decode('utf-8')) #converts payload to json
+
+    print("Sensor: ", final_res["Sensor"])
+    print("Transducer: ", final_res["Transducer"])
 
 if __name__ == "__main__":
-    client = client_connect()
-    subscribe(client)
+    client = connect_mqtt()
+
+    client.on_connect = on_connect #function passed into client
+    client.on_message = on_message #function passed into client
+
+    client.loop_forever()
